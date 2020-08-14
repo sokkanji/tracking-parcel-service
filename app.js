4ccac6e81e76d910d18a4f156455513c67ab7fd6
sokkanji <sskkanji@gmail.com>
2020-08-14 23:41:58 +0900

    fix : aline code

--- app.js
+++ app.js
@@ -2,44 +2,50 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 
-const app=express();
-const port=3000;
+const app = express();
+const port = 3000;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.locals.pretty = true;
 
 app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use((err, req, res, next)=>{
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke');
 });
 
 const t_key = 'qOFWCCHGOUDVU7cf6r5z5w';
-let _t_code='', _t_invoice='', json='';
+let _t_code = '',
+    _t_invoice = '',
+    json = '';
 
-app.get('/', (req, res)=>{
-    res.render('index', {title:'배송 통합 조회 시스템'});
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: '배송 통합 조회 시스템'
+  });
 });
 
-app.post('/tracking', (req, response)=>{
+app.post('/tracking', (req, response) => {
   _t_code = req.body.t_code,
   _t_invoice = req.body.t_invoice;
 
   request.get({
-      uri: 'https://info.sweettracker.co.kr/api/v1/trackingInfo',
-      qs: {
-        't_key': t_key,
-        't_code': _t_code,
-        't_invoice': _t_invoice
-      }
-    }, (err, res, body)=> {
-        json = JSON.parse(body);
-        response.json(json);
-    });  
+    uri: 'https://info.sweettracker.co.kr/api/v1/trackingInfo',
+    qs: {
+      't_key': t_key,
+      't_code': _t_code,
+      't_invoice': _t_invoice
+    }
+  }, (err, res, body) => {
+    json = JSON.parse(body);
+    response.json(json);
+  });
 });
 
-app.listen(port, ()=>{
-    console.log('Connected express server at localhost...');
+app.listen(port, () => {
+  console.log('Connected express server at localhost...');
 });
--- public/js/common.js
+++ public/js/common.js
@@ -1,16 +1,21 @@
-common=()=>{
-    let t_code="", t_invoice="", complete="", level="", str ="", result=[], result2, i;
+common = () => {
+    let t_code = "",
+        t_invoice = "",
+        level = "",
+        str = "",
+        result = [],
+        result2, i;
 
-    $(document).ready(()=>{
-        $('#lookup-btn').click(()=> {
-            t_code=$("input[name='t_code']:checked").val();
-            t_invoice=$("input[name='t_invoice']").val();
+    $(document).ready(() => {
+        $('#lookup-btn').click(() => {
+            t_code = $("input[name='t_code']:checked").val();
+            t_invoice = $("input[name='t_invoice']").val();
             $(".result-tr1 td").remove();
             $(".result-tr2 td").remove();
 
-            if (t_invoice.length == 0){
+            if (t_invoice.length == 0) {
                 alert("운송장 번호를 입력해주세요.");
-            } else{ 
+            } else {
                 $.ajax({
                     url: '/tracking',
                     dataType: 'json',
@@ -19,48 +24,55 @@ common=()=>{
                         't_invoice': t_invoice
                     },
                     type: 'POST',
-                    success: (data)=>{
+                    success: (data) => {
                         console.log(data.result);
                         if (data.result == "Y") {
-                            switch(data.level){
-                                case 1: level="배송준비중";
-                                        break;
-                                case 2: level="집화완료";
-                                        break;
-                                case 3: level="배송중";
-                                        break;
-                                case 4: level="지점도착";
-                                        break;
-                                case 5: level="배송출발";
-                                        break;
-                                case 6: level="배송완료";
-                                        break;
-                                default: level="알 수 없음";
+                            switch (data.level) {
+                                case 1:
+                                    level = "배송준비중";
+                                    break;
+                                case 2:
+                                    level = "집화완료";
+                                    break;
+                                case 3:
+                                    level = "배송중";
+                                    break;
+                                case 4:
+                                    level = "지점도착";
+                                    break;
+                                case 5:
+                                    level = "배송출발";
+                                    break;
+                                case 6:
+                                    level = "배송완료";
+                                    break;
+                                default:
+                                    level = "알 수 없음";
                             }
 
-                            result=[t_invoice, data.receiverName, data.receiverAddr, data.itemName, level];
-                            result2=new Array(new Array(data.trackingDetails.length), new Array(4));
+                            result = [t_invoice, data.receiverName, data.receiverAddr, data.itemName, level];
+                            result2 = new Array(new Array(data.trackingDetails.length), new Array(4));
 
-                            for(i=0; i<data.trackingDetails.length; i++){
-                                result2[i]+=[data.trackingDetails[i].timeString, data.trackingDetails[i].where, data.trackingDetails[i].kind, data.trackingDetails[i].telno];
+                            for (i = 0; i < data.trackingDetails.length; i++) {
+                                result2[i] += [data.trackingDetails[i].timeString, data.trackingDetails[i].where, data.trackingDetails[i].kind, data.trackingDetails[i].telno];
                             }
-                            for(i=0; i<5; i++){ 
-                                $(".result-tr1").append("<td>"+result[i]+"</td>");
+                            for (i = 0; i < 5; i++) {
+                                $(".result-tr1").append("<td>" + result[i] + "</td>");
                             }
-                            for(i=0; i<data.trackingDetails.length; i++){
+                            for (i = 0; i < data.trackingDetails.length; i++) {
                                 str = "<tr class='result-tr2'>" +
-                                            "<td>"+data.trackingDetails[i].timeString+"</td>"+
-                                            "<td>"+data.trackingDetails[i].where+"</td>"+
-                                            "<td>"+data.trackingDetails[i].kind+"</td>"+
-                                            "<td>"+data.trackingDetails[i].telno+"</td>"+
+                                        "<td>" + data.trackingDetails[i].timeString + "</td>" +
+                                        "<td>" + data.trackingDetails[i].where + "</td>" +
+                                        "<td>" + data.trackingDetails[i].kind + "</td>" +
+                                        "<td>" + data.trackingDetails[i].telno + "</td>" +
                                     "</tr>";
                                 $("#t2 tbody").append(str);
                             }
-                        } else{
+                        } else {
                             alert('조회 결과가 없습니다.');
                         }
                     },
-                    error: (xhr, textStatus, errorThrown)=> {
+                    error: (xhr, textStatus, errorThrown) => {
                         alert("조회 실패했습니다.");
                     }
                 });
